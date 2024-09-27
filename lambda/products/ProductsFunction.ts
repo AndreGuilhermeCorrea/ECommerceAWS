72fc09c0fb37f4c2ee4fc74241fb3636d03164f2
AndreGuilhermeCorrea <de_correa@msn.com>
2024-09-27 10:29:29 -0300

    :sparkles: feat: Função Lambda - Captura RequestId

--- lambda/products/ProductsFunction.ts
+++ lambda/products/ProductsFunction.ts
@@ -2,23 +2,29 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { Context } from "vm";
 
 //funcao handler assincrona que será invocada pelo api gateway
-export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
-    const method = event.httpMethod;
-    if(event.resource === "/products") {
-        if(event.httpMethod === "GET") {
-            console.log("GET /products");
-            return {
-                statusCode: 200,
-                body: JSON.stringify({
-                    message: "GET /products",
-                }), 
-            };
+export async function handler(event: APIGatewayProxyEvent, 
+    context: Context): Promise<APIGatewayProxyResult> {
+        //obtem o id da requisição
+        const lambdaRequestId = context.awsRequestId;
+        //obtem o id da requisição http
+        const apiRequestId = event.requestContext.requestId;       
+        //obtem o metodo http
+        const method = event.httpMethod;
+        if(event.resource === "/products") {
+            if(event.httpMethod === "GET") {
+                console.log("GET /products");
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify({
+                        message: "GET /products",
+                    }), 
+                };
+            }
         }
-    }
-        return {
-            statusCode: 400,
-            body: JSON.stringify({
-                message: "Error",
-            }),
-        };
+            return {
+                statusCode: 400,
+                body: JSON.stringify({
+                    message: "Error",
+                }),
+            };
     }
