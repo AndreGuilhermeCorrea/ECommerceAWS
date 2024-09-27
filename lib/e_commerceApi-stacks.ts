39e9fc32512a54e4ef7800cb2553b11c9997eec6
AndreGuilhermeCorrea <de_correa@msn.com>
2024-09-27 11:03:30 -0300

    :sparkles: feat: Stack do API Gateway - Gerador de logs

--- lib/e_commerceApi-stacks.ts
+++ lib/e_commerceApi-stacks.ts
@@ -15,9 +15,29 @@ export class ECommerceApiStack extends cdk.Stack {
     //construtor: recebe o escopo, o id e as propriedades do stack
     constructor(scope: Construct, id: string, props: ECommerceApiStackProps) {
         super(scope, id, props);
+        //pastas que agrupam os arquivos de logs
+        const logGroup = new cwlogs.LogGroup(this, "ECommerceApiLogGroup");
         //cria uma api gateway
         const api = new apigateway.RestApi(this, "ECommerceApi", {
             restApiName: "ECommerceApi",
+            cloudWatchRole: true,
+            deployOptions: {
+                //configurações do deploy
+                accessLogDestination: new apigateway.LogGroupLogDestination(logGroup),
+                //configurações do log
+                accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields({
+                    //campos que serão exibidos no log
+                    httpMethod: true,
+                    ip: true,
+                    protocol: true,
+                    requestTime: true,
+                    resourcePath: true,
+                    responseLength: true,
+                    status: true,
+                    caller: true,
+                    user: true,
+                }),
+            }
         });
         //cria um recurso de integração para os produtos
         const productsFetchIntegration = new apigateway.LambdaIntegration(props.productsFunction);
